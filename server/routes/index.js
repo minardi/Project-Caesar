31d07c6e30ac130c46c6686c5e279a2160836fb6
betasaturday <evgeniya.knm@gmail.com>
2015-12-02 00:01:25 +0200

    Mongolab.com db reset

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,39 +2,72 @@ var express = require('express');
 var mongoose = require('mongoose');
 var router = express.Router();
 var Group = require('../models/Group');
+var GroupMg;
 
 router.get('/', function (req, res) {
-    var staticRoote = '../client';
+    var staticRoute ='../client';
     res.sendFile('index.html', {root: staticRoute});
 });
 
 
 router.get('/group', function(req, res) {
-    var groups = mongoose.model('Group1');
-    /*var groupList = [
-        {name: 'Dp-085 .Net', locationCity: 'Dnipro', status: 'finished'},
-        {name: 'Dp-086 UI', locationCity: 'Dnipro', status: 'current'},
-        {name: 'Lv-171-iOS', locationCity: 'Lviv', status: 'current'}
-    ];
-    groupList.forEach(function (data) {
-        var group = new groups(data);
-        group.save(function(err){
-            if (err) throw err;
-        });
-    });*/
-    groups.find({}, function (err, data) {
-        if(err) throw err;
-        console.log(data)
+    console.log('group');
+    var GroupModel = mongoose.model('Group');
+    
+    GroupModel.find({}, function(err, data) {
+        if (err) {
+            throw err;
+        }    
+        console.log(data);
         res.send(data);
     });
-});
-/* GET home page. */
-/*router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    
 });
 
-router.get('/helloworld', function(req, res) {
-    res.render('helloworld', { title: 'Hello, World!' });
-});*/
+router.get('/reset', function(req, res) {
+    mongoose.connection.db.dropDatabase(function(err, result) {
+        console.log('reset');
+        var GroupModel = mongoose.model('Group');
+        
+        var GroupList = [
+            {
+                title: 'Ssdjfk',
+                startDate: '01.01.2015',
+                finishDate: '03.04.2016',
+                location: 'Dp',
+                direction: 'UI',
+                status: 'In Progress'
+            },
+            {
+                title: 'Tsdfjk',
+                startDate: '02.08.2015',
+                finishDate: '06.09.2016',
+                location: 'Lv',
+                direction: 'DevOps',
+                status: 'Finished'
+            }];
+            
+            GroupList.forEach(function (groupJSON) {
+                var groupInDb = GroupModel(groupJSON);
+                groupInDb.save(function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    
+                });
+            });
+
+        res.render('index', { title: 'Express' });
+    });
+});
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//     res.render('index', { title: 'Express' });
+// });
+
+// router.get('/helloworld', function(req, res) {
+//     res.render('helloworld', { title: 'Hello, World!' });
+// });
 
 module.exports = router;
