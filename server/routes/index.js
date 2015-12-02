0e891dd76a0e4c416adc6e7fd212bc586a6472d3
unknown <D:\Mail>
2015-12-02 21:22:15 +0200

    Location v.3

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var mongoose = require('mongoose');
 var router = express.Router();
 //var Group = require('../models/Group');
-var GroupMg;
+var groupList = require('../reset_data/group-list.js');
+var locationList = require('../reset_data/location-list.js');
 
 require('../models/Location');
 
@@ -42,35 +43,28 @@ router.get('/resetdb', function(req, res, next) {
 router.get('/reset', function(req, res) {
     mongoose.connection.db.dropDatabase(function(err, result) {
         console.log('reset');
-        var GroupModel = mongoose.model('Group');
-        
-        var GroupList = [
-            {
-                title: 'Ssdjfk',
-                startDate: '01.01.2015',
-                finishDate: '03.04.2016',
-                location: 'Dp',
-                direction: 'UI',
-                status: 'In Progress'
-            },
-            {
-                title: 'Tsdfjk',
-                startDate: '02.08.2015',
-                finishDate: '06.09.2016',
-                location: 'Lv',
-                direction: 'DevOps',
-                status: 'Finished'
-            }];
+        var GroupModel = mongoose.model('Group'),
+            LocationModel = mongoose.model('LocationModel');;
             
-            GroupList.forEach(function (groupJSON) {
-                var groupInDb = GroupModel(groupJSON);
-                groupInDb.save(function (err) {
-                    if (err) {
-                        console.log(err);
-                    }
-                    
-                });
+        groupList.forEach(function (groupJSON) {
+            var groupInDb = GroupModel(groupJSON);
+            groupInDb.save(function (err) {
+                if (err) {
+                    console.log(err);
+                }
+                
             });
+        });
+
+        locationList.forEach(function (locationJSON) {
+            var locationInDb = LocationModel(locationJSON);
+            locationInDb.save(function (err) {
+                if (err) {
+                    console.log(err);
+                }
+                
+            });
+        });
 
         res.render('index', { title: 'Express' });
     });
