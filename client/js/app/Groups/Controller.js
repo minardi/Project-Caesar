230f0b81d65486b753bf441193b02c991e019934
Lurieva <lurieva@i.ua>
2015-12-04 09:29:31 +0200

    Navigation groups

--- client/js/app/Groups/Controller.js
+++ client/js/app/Groups/Controller.js
@@ -5,23 +5,73 @@
         this.collectionView = new This.GroupCollectionView();
         this.el = $('.content');
         
-        function renderView() {
-            this.el.children().first().replaceWith(this.collectionView.render().el);
+        function renderView () {
+            this.el.children().first().replaceWith(this.collectionView.renderGroups().el);
         }
-        
+
+        function renderCurrentView () {
+            this.el.append(this.collectionView.renderCurrentGroups().el)
+        }
+
         this.start = function () {
-            renderView.call(this);
+            renderCurrentView.call(this);
+
+            var that = this;
+            var currentView = 'renderCurrent';
+
+            $('#up-navig').on('click', function () {
+                if (currentView === 'renderCurrent') {
+                    cs.mediator.publish('futureGroups');
+                    that.collectionView.renderFutureGroups();
+                    currentView = 'renderFuture';
+                } else if (currentView === 'renderFinished') {
+                    cs.mediator.publish('currentGroups');
+                    that.collectionView.renderCurrentGroups();
+                    currentView = 'renderCurrent';
+                }
+            });
+            $('#down-navig').on('click', function () {
+                if (currentView === 'renderCurrent') {
+                    cs.mediator.publish('finishedGroups');
+                    that.collectionView.renderFinishedGroups();
+                    currentView = 'renderFinished';
+                } else if (currentView === 'renderFuture') {
+                    cs.mediator.publish('currentGroups');
+                    that.collectionView.renderCurrentGroups();
+                    currentView = 'renderCurrent';
+                }
+            });
+
+            setupMediator();
         };
-        
+
         this.showAll = function () {
             this.collection.fetch()
-                .done(renderView.bind(this));
+                .done(renderCurrentView.bind(this));
         };
         
         this.showInLocation = function (location) {
             this.collection.fetch({data: {location: location}})
-                .done(renderView.bind(this));
+                .done(renderCurrentView.bind(this));
         };
+
+            function setupMediator () {
+                cs.mediator.subscribe('currentGroupsView', showCurrentGroups);
+                cs.mediator.subscribe('futureGroupsView', showFutureGroups);
+                cs.mediator.subscribe('finishedGroupsView', showFinishedGroups);
+            }
+
+            function showCurrentGroups () {
+                this.collectionView.renderCurrentGroups();
+            }
+
+            function showFutureGroups () {
+                this.collectionView.renderFutureGroups();
+            }
+
+            function showFinishedGroups () {
+                this.collectionView.renderFinishedGroups();
+            }
         
         return this;
     };
