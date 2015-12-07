c3c041f19cb87b8bf0064d6a063e15f0400ea9a4
Sanya Poltorak <D:\Mail>
2015-12-08 00:18:10 +0200

    del unnecessary method

--- client/js/app/Locations/model/LocationCollection.js
+++ client/js/app/Locations/model/LocationCollection.js
@@ -2,16 +2,6 @@
 (function (This) {
     This.LocationCollection = Backbone.Collection.extend({
         model: This.Location,
-        url: '/dbLocations',
-
-        search: function (searchString) {
-            var pattern = new RegExp(searchString,"gi");
-
-            if (searchString == "") return this.toArray();
-     
-            return this.filter(function(model) {
-                return pattern.test(model.get("city"));
-            });
-        }
+        url: '/dbLocations'
     });
 })(App.Locations);
