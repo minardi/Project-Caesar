6ea8f9fd2defcbd4e5d691b559456ba4f39942b2
Dimon <gunko@ukr.net>
2015-12-08 16:38:16 +0200

    group routing refactoring and style fix

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -9,4 +9,63 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.get('/groups', function(req, res, next) {
+    var Groups = mongoose.model('Group'), 
+        options = {};
+
+    if (req.query['location']) {
+        options['location'] = req.query['location'];
+    };
+    
+    Groups.find(options, function(err, data) {
+        if (err) {throw err};   
+
+        res.send(data);
+    });
+});
+
+router.delete('/group/:id', function (req, res, next) {
+    var Group = mongoose.model('Group');
+    Group.remove({_id: req.params.id}, function(err) {
+      if (err) {throw err};
+    });
+    res.json({status: 'success'});
+});
+
+router.post('/group', function (req, res, next) {
+    var Group = mongoose.model('Group'),
+        newGroup = new Group({     
+            name: req.body.name,
+            direction: req.body.direction,
+            location: req.body.location,
+            startDate: req.body.startDate,
+            finishDate: req.body.finishDate,
+            status: req.body.status,
+            teachers: req.body.teachers,
+            experts: req.body.experts
+        });
+
+    newGroup.save(function(err, data) {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+router.put('/group/:id', function (req, res, next) {
+    var Group = mongoose.model('Group');
+    console.log(req.body);
+    Group.findOneAndUpdate({_id:req.params.id}, req.body, function (err) {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else {
+            res.json(req.body);
+        }
+    });
+});
+
 module.exports = router;
