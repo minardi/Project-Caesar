53dcc86fc9952106a499e25ab04b3e57e30c477d
DmitryShashkov <dmitry.the.wise@gmail.com>
2015-12-12 12:44:42 +0200

    Schedule refactored

--- server/reset_data/event-list.js
+++ server/reset_data/event-list.js
@@ -3,73 +3,73 @@ var moment = require('moment');
 var eventList = [
     {
         type: 'Practics',
-        dateTime: moment(),
+        dateTime: moment().hour(8).minute(0),
         groupID: '5669cb7c723b4830166eaea5',
         duration: 4
     },
     {
         type: 'Lecture',
-        dateTime: moment().add(1, 'd'),
+        dateTime: moment().hour(8).minute(30).add(1, 'd'),
         groupID: '5669cb7c723b4830166eaea5',
         duration: 4
     },
     {
         type: 'Consulting',
-        dateTime: moment().add(2, 'd'),
+        dateTime: moment().hour(10).minute(0).add(2, 'd'),
         groupID: '5669cb7c723b4830166eaea5',
         duration: 4
     },
     {
         type: 'Practics',
-        dateTime: moment().add(3, 'd'),
+        dateTime: moment().hour(10).minute(30).add(3, 'd'),
         groupID: '5669cb7c723b4830166eaea5',
         duration: 4
     },
     {
         type: 'Lecture',
-        dateTime: moment().add(4, 'd'),
+        dateTime: moment().hour(12).minute(0).add(4, 'd'),
         groupID: '5669cb7c723b4830166eaea5',
         duration: 4
     },
     {
         type: 'Consulting',
-        dateTime: moment().add(5, 'd'),
+        dateTime: moment().hour(12).minute(30).add(5, 'd'),
         groupID: '5669cb7c723b4830166eaea5',
         duration: 4
     },
     {
         type: 'Practics',
-        dateTime: moment().add(6, 'd'),
+        dateTime: moment().hour(14).minute(0).add(6, 'd'),
         groupID: '5669cb7c723b4830166eaea5',
         duration: 4
     },
     {
         type: 'Lecture',
-        dateTime: moment().add(7, 'd'),
+        dateTime: moment().hour(14).minute(30).add(7, 'd'),
         groupID: '5669cb7c723b4830166eaea5',
         duration: 4
     },
     {
         type: 'Consulting',
-        dateTime: moment().add(8, 'd'),
+        dateTime: moment().hour(16).minute(0).add(8, 'd'),
         groupID: '5669cb7c723b4830166eaea5',
         duration: 4
     },
     {
         type: 'Practics',
-        dateTime: moment().add(9, 'd'),
+        dateTime: moment().hour(16).minute(30).add(9, 'd'),
         groupID: '5669cb7c723b4830166eaea5',
         duration: 4
     },
     {
         type: 'Lecture',
-        dateTime: moment().add(10, 'd'),
+        dateTime: moment().hour(18).minute(00).add(10, 'd'),
         groupID: '5669cb7c723b4830166eaea5',
         duration: 4
     },
     {
         type: 'Consulting',
-        dateTime: moment().add(11, 'd'),
+        dateTime: moment().hour(18).minute(30).add(11, 'd'),
         groupID: '5669cb7c723b4830166eaea5',
         duration: 4
     }
