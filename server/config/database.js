9d9d2456225e8cb96a585eb7dfb8bb6d2fcfe81a
Lurieva <lurieva@i.ua>
2015-12-14 20:23:40 +0200

    local database

--- server/config/database.js
+++ server/config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://test:test@ds063134.mongolab.com:63134/caesar'
+    //url: 'mongodb://test:test@ds063134.mongolab.com:63134/caesar'
+    url: 'mongodb://127.0.0.1/caesar'
 };
