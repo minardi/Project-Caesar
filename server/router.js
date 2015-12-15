5d95a3261ae912f2fd056caa4238bac376bb3ba1
Dimon <gunko@ukr.net>
2015-12-15 17:02:35 +0200

    Module validation of CRUD operations (make it global)

--- server/router.js
+++ server/router.js
@@ -6,8 +6,9 @@ var express = require('express'),
     continueSession = require('./routes/continueSession'),
     rest = require('./routes/rest'),
     openPage = require('./routes/openPage'),
+    userAcess = require('./routes/userAcess')
     app = express();
-    
+
 app.use('/', routes);
 app.use('/reset', reset);
 app.use('/rest', rest);
@@ -20,5 +21,34 @@ app.use('*', function(req, res){
     res.sendFile('home.html', {root: staticRoute});
 });
 
+routes.post('*', function (req, res, next) {
+    userAcess.userAcess(req, function(result){
+        if(result) {
+            next();
+        } else {
+            console.log('Post Forbiden');
+        };
+    });
+});
+
+routes.post('*', function (req, res, next) {
+    userAcess.userAcess(req, function(result){
+        if(result) {
+            next();
+        } else {
+            console.log('Update Forbiden');
+        };
+    });
+});
+
+routes.delete('*', function (req, res, next) {
+    userAcess.userAcess(req, function(result){
+        if(result) {
+            next();
+        } else {
+            console.log('Delete Forbiden');
+        };
+    });
+});
 
 module.exports = app;
