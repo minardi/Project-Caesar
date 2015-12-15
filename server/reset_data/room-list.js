8587d286d8afa47045d18c007c7708f834c48f37
betasaturday <evgeniya.knm@gmail.com>
2015-12-15 19:55:36 +0200

    Add routes

--- server/reset_data/room-list.js
+++ server/reset_data/room-list.js
@@ -1,24 +1,34 @@
 var roomList = [
-	{
-		_id: 0,
-		name: '703',
-		office: 0
-	},
-	{
-		_id: 1,
-		name: '704',
-		office: 0
-	},
-	{
-		_id: 2,
-		name: '500',
-		office: 2
-	},
-	{
-		_id: 3,
-		name: '600',
-		office: 3
-	}
+    {
+        _id: 0,
+        name: '703',
+        office: 0
+    },
+    {
+        _id: 1,
+        name: '704',
+        office: 0
+    },
+    {
+        _id: 2,
+        name: '500',
+        office: 2
+    },
+    {
+        _id: 3,
+        name: '600',
+        office: 3
+    },
+    {
+        _id: 4,
+        name: '1034',
+        office: 1
+    },
+    {
+        _id: 5,
+        name: '122',
+        office: 1
+    },
 ];
 
 module.exports = roomList;
