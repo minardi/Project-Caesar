f40f23433ba2a461cf1ce49c9f99f6549bfbf265
Dimon <gunko@ukr.net>
2015-12-17 12:44:59 +0200

    Contributors view update

--- client/js/app/Contributors/tpl/ContributorTpl.js
+++ client/js/app/Contributors/tpl/ContributorTpl.js
@@ -1,14 +1,15 @@
 templates.contributorTpl = _.template([
-    '<div class="content-item">',
-        '<div class="fotoContributor">Foto',
-        '</div>',
-        '<div class="infoContributor">' +
-            '<h4><%= name %> <%= lastName %></h4>',
+    '<div class="content-item contributor-item">',
+        '<div class="media">',
+          '<div class="media-left">',
+            '<a href="#">',
+              '<img width="100" height="100" class="media-object" src="" alt="">',
+            '</a>',
+          '</div>',
+          '<div class="media-body">',
+            '<h4 class="media-heading"><%= name %> <%= lastName %></h4>',
             '<p><%= team %></p>',
+          '</div>',
         '</div>',
-        /*'<div class="infoContributor">',
-            '<h4><%= name %> <%= lastName %></h4>',
-            '<p><%= team %></p>',
-        '</div>',*/
     '</div>'
 ].join(''));
