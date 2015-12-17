780733071e818ad2835af90768a49191cb973d61
Lurieva <lurieva@i.ua>
2015-12-17 12:04:08 +0200

    contributor css

--- client/js/app/Contributors/tpl/ContributorTpl.js
+++ client/js/app/Contributors/tpl/ContributorTpl.js
@@ -2,9 +2,13 @@ templates.contributorTpl = _.template([
     '<div class="content-item">',
         '<div class="fotoContributor">Foto',
         '</div>',
-        '<div class="content-wrap">',
+        '<div class="infoContributor">' +
             '<h4><%= name %> <%= lastName %></h4>',
             '<p><%= team %></p>',
         '</div>',
+        /*'<div class="infoContributor">',
+            '<h4><%= name %> <%= lastName %></h4>',
+            '<p><%= team %></p>',
+        '</div>',*/
     '</div>'
 ].join(''));
